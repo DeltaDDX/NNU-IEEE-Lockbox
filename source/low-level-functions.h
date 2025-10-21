ff4a722ff937fb369359cc00ec27a773f249a07c
mWilloughby21 <madduxwilloughby2121@gmail.com>
2025-10-20 19:58:39 -0600

    Implement LCD printing and servo unlock functions
    Added functions to print messages to LCD and unlock a box using a servo.

--- source/low-level-functions.h
+++ source/low-level-functions.h
@@ -26,6 +26,7 @@ void rfid_setup() {
 
 // ========================= Functions =========================
 
+// Compare RFID -> unlock box
 void checkRFID() {
   if (mfrc522.uid.uidByte[0] != nuidPICC[0] ||
       mfrc522.uid.uidByte[1] != nuidPICC[1] ||
@@ -45,13 +46,56 @@ void checkRFID() {
 #endif
 
   } 
-
     servo_unlock();
     /*
     rainbowCycle(10);
     strip.clear();
     strip.show();
     */
-  
 }
 
+// Print to lcd screen
+void lcd_print(String output) {
+   lcd.clear();
+   lcd.setCursor(0, 0);
+
+   if (output.length() >= 16) {
+    lcd.print(output.substring(0,16));
+    lcd.setCursor(0, 1);
+    lcd.print(output.substring(16, output.length()));
+  } else {
+    lcd.setCursor(0, 0);
+    lcd.print(output);
+  }
+}
+
+// Unlock box
+void servo_unlock() {
+  lockServo.write(SERVO_UNLOCK_ANGLE);
+  lcd_print("Unlocked");
+  delay(HOLD_MS);
+  lockServo.write(SERVO_LOCK_ANGLE);
+  lcd_print("Locked");
+}
+
+
+// future idea
+/*
+struct uid {
+  byte uidBytes[4];
+};
+
+bool compare_uid(uid uid1, uid uid2) {
+  for (byte i = 0; i < len(uid1.uidBytes); ++i) {
+    if (uid1.uidBytes[i] != uid2.uidBytes[i]) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+uid make_uid() {
+  
+}
+*/
